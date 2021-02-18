12fdc9394c66c27b234e1ee97dd34f4685692647
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 20:30:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-alan_kay_is_known_for[:alan_kay][:known_for]
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
