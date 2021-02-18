d15652946d670ea84ce7a0916080bbf2cec0cbec
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 20:51:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto] = [:known_for][:languages]
-
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 end
 
 def changing_alan
