8e0388215c35ea76efee174f0e2fbce989973d79
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 20:49:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,11 +80,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-programmer_hash
+programmer_hash[:yukihiro_matsumoto] = [:known_for][:languages]
 end
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
