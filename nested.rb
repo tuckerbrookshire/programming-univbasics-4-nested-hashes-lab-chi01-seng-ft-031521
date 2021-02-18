048d0867503de7d83da122ac77760b92f80ea9ee
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 20:58:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages] =+ "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
